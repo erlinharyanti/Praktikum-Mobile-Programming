945a238c7a9d1f691fe633b0ac08a5c835c3b57b
erlinharyanti <rlindwih2005@gmail.com>
2025-09-25 12:49:24 +0700

    add praktikum 4

---
+++ praktikum4/lib/main.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Halaman Formulir',
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home: const FormMahasiswaPage(),
+    );
+  }
+}
+
+class FormMahasiswaPage extends StatefulWidget {
+  const FormMahasiswaPage({super.key});
+
+  @override
+  State<FormMahasiswaPage> createState() => _FormMahasiswaPageState();
+}
+
+class _FormMahasiswaPageState extends State<FormMahasiswaPage> {
+  final _formKey = GlobalKey<FormState>();
+  final cNama = TextEditingController();
+  final cNpm = TextEditingController();
+  final cEmail = TextEditingController();
+  final cAlamat = TextEditingController();
+  final cNoHp = TextEditingController();
+
+  //boleh null
+  DateTime? tglLahir;
+  TimeOfDay? jamBimbingan;
+
+  String get tglLahirLabel => tglLahir == null
+      ? 'Pilih Tanggal Lahir' //? turnary if else
+      : '${tglLahir!.day}/${tglLahir!.month}/${tglLahir!.year}';
+  String get jamLabel => jamBimbingan == null
+      ? 'Pilih Jam Bimbingan'
+      : '${jamBimbingan!.hour}:${jamBimbingan!.minute}';
+  String? jenisKelamin;
+
+  @override
+  void dispose() {
+    cNama.dispose();
+    cNpm.dispose();
+    cEmail.dispose();
+    cAlamat.dispose();
+    cNoHp.dispose();
+    super.dispose();
+  }
+
+  Future<void> _pickDate() async {
+    //async berhubungan sm await
+    final res = await showDatePicker(
+      context: context,
+      firstDate: DateTime(2000), //tgl pertama
+      initialDate: DateTime.now(), //tgl akhir
+      lastDate: DateTime(
+        DateTime.now().year + 1,
+      ), //waktu default tgl sekarang + 1 taun kedepan
+    );
+    if (res != null) setState(() => tglLahir = res); //res ini berisi apa ngga, kalo berisi dia set state
+  }
+
+  Future<void> _pickTime() async {
+    final res = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+    if (res != null) setState(() => jamBimbingan = res);
+  }
+
+  void _simpan(){
+    if(!_formKey.currentState!.validate()|| tglLahir == null || jamBimbingan == null){
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Data belum lengkap!')),
+      );
+      return;
+    }
+    showDialog(context: context, builder: (_) => AlertDialog(
+      title: const Text('Ringkasan Data'),
+      content: Text(
+        'Nama: ${cNama.text}\nEmail: ${cEmail.text}\nNPM: ${cNpm.text}\nAlamat: ${cAlamat.text}\nNo HP: ${cNoHp.text}\nJenis Kelamin: $jenisKelamin\nTanggal Lahir: $tglLahirLabel\nJam Bimbingan: $jamLabel',
+      ),
+    ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(title: const Text('Formulir Mahasiswa')),
+    body: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          children: [
+            TextFormField(
+              controller: cNama,
+              decoration: const InputDecoration(labelText: 'Nama', prefixIcon: Icon(Icons.person)),
+              validator: (value) =>
+              value == null || value.isEmpty ? 'Nama harus diisi' : null,
+            ),
+            TextFormField(
+              controller: cNpm,
+              decoration: const InputDecoration(labelText: 'NPM', prefixIcon: Icon(Icons.badge)),
+              keyboardType: TextInputType.number,
+              validator: (value) =>
+              value == null || value.isEmpty ? 'NPM harus diisi' : null,
+            ),
+            TextFormField(
+              controller: cEmail,
+              decoration: const InputDecoration(labelText: 'Email', prefixIcon: Icon(Icons.email)),
+              keyboardType: TextInputType.emailAddress,
+              validator: (v) {
+                if (v == null || v.isEmpty) return 'Email harus diisi';
+                final ok = RegExp(r'^[^@]+@unsika\.ac\.id$').hasMatch(v.trim());
+                return ok ? null : 'Email harus domain @unsika.ac.id';
+              },
+            ),
+            TextFormField(
+              controller: cAlamat,
+              decoration: const InputDecoration(labelText: 'Alamat', prefixIcon: Icon(Icons.home)),
+              maxLines: 3,
+              validator: (value) =>
+              value == null || value.isEmpty ? 'Alamat harus diisi' : null,
+            ),
+            TextFormField(
+              controller: cNoHp,
+              decoration: const InputDecoration(labelText: 'Nomor HP', prefixIcon: Icon(Icons.phone)),
+              keyboardType: TextInputType.phone,
+              validator: (value) {
+                if (value == null || value.isEmpty) return 'Nomor HP harus diisi';
+                if (!RegExp(r'^\d+$').hasMatch(value)) return 'Nomor HP harus berupa angka';
+                if (value.length < 10) return 'Nomor HP minimal 10 digit';
+                return null;
+              },
+            ),
+            DropdownButtonFormField<String>(
+              decoration: const InputDecoration(labelText: 'Jenis Kelamin'),
+              initialValue: jenisKelamin,
+              items: const [
+                DropdownMenuItem(value: 'Laki-laki', child: Text('Laki-laki')),
+                DropdownMenuItem(value: 'Perempuan', child: Text('Perempuan')),
+              ],
+              onChanged: (v) => setState(() => jenisKelamin = v),
+              validator: (value) =>
+              value == null || value.isEmpty ? 'Jenis kelamin harus dipilih' : null,
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: _pickDate,
+              child: Text(tglLahirLabel),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: _pickTime,
+              child: Text(jamLabel),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton.icon(
+              onPressed: _simpan,
+              icon: const Icon(Icons.save),
+              label: const Text('Simpan'),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
